2a70ced1634b92bd6a6c19dce418db60e120a8e6
joegov <joe@datamaster.net.au>
2016-07-31 13:09:53 +1000

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,276 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnCopyPublic = New System.Windows.Forms.Button()
+        Me.btnCopyPrivate = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPublicKey = New System.Windows.Forms.TextBox()
+        Me.txtPrivatekey = New System.Windows.Forms.TextBox()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.btnRegister = New System.Windows.Forms.Button()
+        Me.btnVote = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.LightGray
+        Me.Panel1.Controls.Add(Me.btnCopyPublic)
+        Me.Panel1.Controls.Add(Me.btnCopyPrivate)
+        Me.Panel1.Controls.Add(Me.TextBox5)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.txtPublicKey)
+        Me.Panel1.Controls.Add(Me.txtPrivatekey)
+        Me.Panel1.Controls.Add(Me.btnGenerate)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel1.Location = New System.Drawing.Point(35, 191)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(426, 387)
+        Me.Panel1.TabIndex = 0
+        '
+        'btnCopyPublic
+        '
+        Me.btnCopyPublic.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCopyPublic.ForeColor = System.Drawing.Color.Green
+        Me.btnCopyPublic.Location = New System.Drawing.Point(356, 228)
+        Me.btnCopyPublic.Name = "btnCopyPublic"
+        Me.btnCopyPublic.Size = New System.Drawing.Size(35, 19)
+        Me.btnCopyPublic.TabIndex = 9
+        Me.btnCopyPublic.Text = "Copy"
+        Me.btnCopyPublic.UseVisualStyleBackColor = True
+        '
+        'btnCopyPrivate
+        '
+        Me.btnCopyPrivate.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCopyPrivate.ForeColor = System.Drawing.Color.Red
+        Me.btnCopyPrivate.Location = New System.Drawing.Point(357, 171)
+        Me.btnCopyPrivate.Name = "btnCopyPrivate"
+        Me.btnCopyPrivate.Size = New System.Drawing.Size(35, 19)
+        Me.btnCopyPrivate.TabIndex = 8
+        Me.btnCopyPrivate.Text = "Copy"
+        Me.btnCopyPrivate.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.Color.LightGray
+        Me.TextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox5.Location = New System.Drawing.Point(56, 293)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(320, 69)
+        Me.TextBox5.TabIndex = 7
+        Me.TextBox5.Text = "You may tap the Generate Button as many times as you like, please use the last pa" & _
+    "ir you have selected (please use copy paste to add the public key to your voting" & _
+    " or census form )"
+        Me.TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Green
+        Me.Label2.Location = New System.Drawing.Point(40, 231)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(291, 12)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Public key - use when completing your VOTE or CENSUS"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(39, 173)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(289, 12)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Private key - use only on your personal registration page"
+        '
+        'txtPublicKey
+        '
+        Me.txtPublicKey.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtPublicKey.Location = New System.Drawing.Point(39, 251)
+        Me.txtPublicKey.Name = "txtPublicKey"
+        Me.txtPublicKey.Size = New System.Drawing.Size(353, 21)
+        Me.txtPublicKey.TabIndex = 4
+        '
+        'txtPrivatekey
+        '
+        Me.txtPrivatekey.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtPrivatekey.Location = New System.Drawing.Point(39, 193)
+        Me.txtPrivatekey.Name = "txtPrivatekey"
+        Me.txtPrivatekey.Size = New System.Drawing.Size(354, 21)
+        Me.txtPrivatekey.TabIndex = 3
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnGenerate.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGenerate.Location = New System.Drawing.Point(40, 84)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(353, 67)
+        Me.btnGenerate.TabIndex = 2
+        Me.btnGenerate.Text = "Generate your Public and Private Key"
+        Me.btnGenerate.UseVisualStyleBackColor = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.LightGray
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.TextBox2.Location = New System.Drawing.Point(3, 50)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(420, 28)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.TabStop = False
+        Me.TextBox2.Text = "PROTECTING YOUR PRIVACY"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnRegister
+        '
+        Me.btnRegister.Location = New System.Drawing.Point(12, 713)
+        Me.btnRegister.Name = "btnRegister"
+        Me.btnRegister.Size = New System.Drawing.Size(31, 24)
+        Me.btnRegister.TabIndex = 11
+        Me.btnRegister.Text = "R"
+        Me.btnRegister.UseVisualStyleBackColor = True
+        '
+        'btnVote
+        '
+        Me.btnVote.Location = New System.Drawing.Point(449, 713)
+        Me.btnVote.Name = "btnVote"
+        Me.btnVote.Size = New System.Drawing.Size(31, 24)
+        Me.btnVote.TabIndex = 10
+        Me.btnVote.Text = "V"
+        Me.btnVote.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.Vote_Form
+        Me.Panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel3.Location = New System.Drawing.Point(12, 24)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(468, 683)
+        Me.Panel3.TabIndex = 2
+        Me.Panel3.Visible = False
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.TextBox6)
+        Me.Panel2.Controls.Add(Me.TextBox1)
+        Me.Panel2.Location = New System.Drawing.Point(35, 188)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(426, 31)
+        Me.Panel2.TabIndex = 1
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox6.Location = New System.Drawing.Point(397, 2)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(29, 26)
+        Me.TextBox6.TabIndex = 0
+        Me.TextBox6.Text = "X"
+        Me.TextBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(45, 9)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(339, 15)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Ausralian Federal Election 2019 - Privacy Wallet"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Panel4
+        '
+        Me.Panel4.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.PrivateData
+        Me.Panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel4.Location = New System.Drawing.Point(20, 24)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(449, 332)
+        Me.Panel4.TabIndex = 3
+        Me.Panel4.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.Phone4s
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(492, 753)
+        Me.Controls.Add(Me.btnRegister)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.btnVote)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.DoubleBuffered = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Key Pair"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtPublicKey As System.Windows.Forms.TextBox
+    Friend WithEvents txtPrivatekey As System.Windows.Forms.TextBox
+    Friend WithEvents btnGenerate As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents btnCopyPublic As System.Windows.Forms.Button
+    Friend WithEvents btnCopyPrivate As System.Windows.Forms.Button
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents btnVote As System.Windows.Forms.Button
+    Friend WithEvents btnRegister As System.Windows.Forms.Button
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,81 @@
+﻿
+Imports System.Security.Cryptography
+
+Public Class Form1
+
+    Private Sub TextBox6_TextChanged(sender As System.Object, e As System.EventArgs) Handles TextBox6.TextChanged
+        'Me.Close()
+        Application.Exit()
+    End Sub
+
+
+    Private Sub btnGenerate_Click(sender As System.Object, e As System.EventArgs) Handles btnGenerate.Click
+
+        'Generate a public/private key pair.
+        Dim RSA As RSACryptoServiceProvider = New RSACryptoServiceProvider()
+
+        Dim RSAKeyInfo As RSAParameters = RSA.ExportParameters(True) ' true gets public and private keys
+        txtPublicKey.Text = Convert.ToBase64String(RSAKeyInfo.Modulus)
+
+        txtPublicKey.Text = "e=" + Convert.ToBase64String(RSAKeyInfo.Exponent) + ", n=" + Convert.ToBase64String(RSAKeyInfo.Modulus)
+        txtPrivatekey.Text = "d=" + Convert.ToBase64String(RSAKeyInfo.D) + ", n=" + Convert.ToBase64String(RSAKeyInfo.Modulus)
+
+
+
+    End Sub
+
+
+
+    Private Sub btnCopyPrivate_Click(sender As System.Object, e As System.EventArgs) Handles btnCopyPrivate.Click
+
+        If txtPrivatekey.Text = "" Then MsgBox("Please generate your privacy keys first") : Exit Sub
+
+        MsgBox("Please paste this into your personal details form")
+
+        btnGenerate.Enabled = False
+        btnGenerate.Visible = False
+
+        If txtPrivatekey.Text <> String.Empty Then
+            Clipboard.SetText(txtPrivatekey.Text)
+        Else
+            Clipboard.Clear()
+        End If
+
+
+    End Sub
+
+    Private Sub btnCopyPublic_Click(sender As System.Object, e As System.EventArgs) Handles btnCopyPublic.Click
+
+        If txtPublicKey.Text = "" Then MsgBox("Please generate your privacy keys first") : Exit Sub
+
+        MsgBox("Please paste this into your public voting/census form")
+
+        btnGenerate.Enabled = False
+        btnGenerate.Visible = False
+
+        If txtPublicKey.Text <> String.Empty Then
+            Clipboard.SetText(txtPublicKey.Text)
+        Else
+            Clipboard.Clear()
+        End If
+
+
+
+    End Sub
+
+    Private Sub btnVote_Click(sender As System.Object, e As System.EventArgs) Handles btnVote.Click
+
+        If Panel3.Visible = True Then Panel3.Visible = False : Exit Sub
+
+        Panel3.Visible = True
+
+    End Sub
+
+    Private Sub btnRegister_Click(sender As System.Object, e As System.EventArgs) Handles btnRegister.Click
+
+        If Panel4.Visible = True Then Panel4.Visible = False : Exit Sub
+
+        Panel4.Visible = True
+
+    End Sub
+End Class
